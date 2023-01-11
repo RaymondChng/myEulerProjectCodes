219fe9af49854bb799399d4b801f96c23b539e66
RaymondChng <122417409+RaymondChng@users.noreply.github.com>
2023-01-11 16:31:59 +0800

    Update myProjectEuler001.py

--- myProjectEuler001.py
+++ myProjectEuler001.py
@@ -2,14 +2,11 @@
 # The sum of these multiples is 23.
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
-
 def IsMultipleOfThreeOrFive(num):
     if num % 3 == 0 and num % 5 == 0:
         return num
-
     elif num % 3 == 0:
         return num
-
     elif num % 5 == 0:
         return num
     else:
